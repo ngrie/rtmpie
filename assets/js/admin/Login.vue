aef0285c0c1af74a39eac4ce41cf3258b03c1772
Niklas Grießer <niklas@griesser.me>
2020-05-16 11:53:24 +0200

    Add error message for failed logins

--- assets/js/admin/Login.vue
+++ assets/js/admin/Login.vue
@@ -35,19 +35,35 @@
         </div>
       </form>
     </div>
+
+    <Notification error :value="loginFailed" @input="loginFailed = false">
+      <template #icon>
+        <svg class="h-5 w-5 text-red-400" fill="currentColor" viewBox="0 0 20 20">
+          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
+        </svg>
+      </template>
+      <template #title>
+        Login failed
+      </template>
+
+      Please check your credentials and try again.
+    </Notification>
   </div>
 </template>
 
 <script>
   import { mapActions } from 'vuex'
+  import Notification from 'ui/notifications/Notification'
 
   export default {
     name: 'Login',
+    components: { Notification },
     data() {
       return {
         username: null,
         password: null,
         loading: false,
+        loginFailed: false,
       }
     },
     mounted() {
@@ -62,11 +78,13 @@
       async login() {
         const { username, password } = this
 
+        this.loginFailed = false
         this.loading = true
         try {
           await this.storeLogin({ username, password })
           this.$router.push('/')
         } catch (e) {
+          this.loginFailed = true
         } finally {
           this.loading = false
         }
